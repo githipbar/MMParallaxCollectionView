01e217f1ef3c1e851b1583e5e6be158c8d4bc321
1ok1 <50324510+1ok1@users.noreply.github.com>
2020-06-22 03:24:56 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MMParallaxCollectionView",
-            dependencies: []),
+            dependencies: [], path: "MMParallaxCollectionView"),
         .testTarget(
             name: "MMParallaxCollectionViewTests",
             dependencies: ["MMParallaxCollectionView"]),
