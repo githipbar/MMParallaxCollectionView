4b3a63b49f8c381a1b007e019b5e09e240bc38e9
1ok1 <50324510+1ok1@users.noreply.github.com>
2020-06-21 18:07:41 +0530

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// 1
+// swift-tools-version:5.0
+// 2
+import PackageDescription
+
+// 3
+let package = Package(
+  // 4
+  name: "MMParallaxCollectionView",
+  // 5
+  platforms: [.iOS(.v13), .macOS(.v10_14)],
+  // 6
+  products: [
+    .library(name: "MMParallaxCollectionView", targets: ["MMParallaxCollectionView"])
+  ],
+  // 7
+  dependencies: [
+    .package(url: "https://github.com/hipbar/MMParallaxCollectionView.git", from: "0.1.6"),
+  ]
+  // 8
+  targets: [
+    .target(name: "MMParallaxCollectionView")
+  ]
+)
