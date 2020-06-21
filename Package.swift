402bdbb14da510235fb5e44ab51041f8c1f5ab1b
1ok1 <50324510+1ok1@users.noreply.github.com>
2020-06-21 19:20:37 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,23 +2,18 @@
 //
 import PackageDescription
 
-// 3
 let package = Package(
-  // 4
   name: "MMParallaxCollectionView",
-  // 5
   platforms: [.iOS(.v11), .macOS(.v10_14)],
-  // 6
   products: [
     .library(name: "MMParallaxCollectionView", targets: ["MMParallaxCollectionView"])
   ],
-  // 7
   dependencies: [
     .package(url: "https://github.com/hipbar/MMParallaxCollectionView.git", from: "0.1.7"),
   ],
   // 8
   targets: [
-    .target(name: "MMParallaxCollectionView", path: "Source"))
+    .target(name: "MMParallaxCollectionView", path: "Source"),
   ],
   swiftLanguageVersions: [.v5]
 )
