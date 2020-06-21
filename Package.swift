8da3db56ebd15d75153e32898bf2497c7638281e
Lokesh <lokesh@hipbar.com>
2020-06-21 19:30:10 +0530

    update: package.swift

--- Package.swift
+++ Package.swift
@@ -3,17 +3,18 @@
 import PackageDescription
 
 let package = Package(
-  name: "MMParallaxCollectionView",
-  platforms: [.iOS(.v11), .macOS(.v10_14)],
-  products: [
-    .library(name: "MMParallaxCollectionView", targets: ["MMParallaxCollectionView"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/hipbar/MMParallaxCollectionView.git", from: "0.1.7"),
-  ],
-  // 8
-  targets: [
-    .target(name: "MMParallaxCollectionView", path: "Source"),
-  ],
-  swiftLanguageVersions: [.v5]
+    name: "MMParallaxCollectionView",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(name: "MMParallaxCollectionView", targets: ["MMParallaxCollectionView"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/hipbar/MMParallaxCollectionView.git", from: "0.1.10"),
+    ],
+    targets: [
+        .target(name: "MMParallaxCollectionView"),
+        .testTarget(name: "MMParallaxCollectionViewTests", dependencies: ["MMParallaxCollectionView"]),
+    ]
 )
